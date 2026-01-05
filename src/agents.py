a6aad568ca9ef377180d7686212e2b6c11a14c2d
PereBlaise2 <nathan.francois.2001@gmail.com>
2026-01-05 16:30:07 +0100

    ajout eta

--- src/agents.py
+++ src/agents.py
@@ -5,15 +5,17 @@ from BMmodel import *
 class AspirationBMAgent:
     """
     Aspiration-based Bush-Mosteller (BM) learner.
-        1) select_action(): uses a noisy version of p (misimplementation epsilon) to sample C/D
-        2) update(reward): updates p using Eq.(2) stimulus + Eq.(1) BM update
+        1) select_action() : uses a noisy version of p (misimplementation epsilon) to sample C/D
+        2) update(reward) : updates p using Eq.(2) stimulus + Eq.(1) BM update
     """
 
-    def __init__(self, A, beta, epsilon, p_init=0.5):
-        self.A = float(A)
+    def __init__(self, A, beta, epsilon, p_init=0.5, eta : float = 0.0):
+        self.A_init = float(A)
+        self.A = self.A_init
         self.beta = float(beta)
         self.epsilon = float(epsilon)
         self.p_init = float(p_init)
+        self.eta = eta
 
         self.p = fix01(float(p_init)) 
         self.last_action = None # we initialize it here to None because it's needed for the BM update (Eq. 1)
@@ -48,15 +50,20 @@ class AspirationBMAgent:
             A=self.A,
             beta=self.beta,
         )
+        
+        if self.eta > 0:
+            self.A = (1 - self.eta) * self.A + self.eta * reward
 
     def reset(self):
         self.p = fix01(float(self.p_init))
         self.last_action = None
+        self.A = self.A_init
 
 
 def make_agents(n_agents, A, beta, epsilon, p_init=0.5):
     """Create a list of identical AspirationBMAgents."""
     return [AspirationBMAgent(A=A, beta=beta, epsilon=epsilon, p_init=p_init) for _ in range(n_agents)]
+
 
 
 # Agent class for PGG game (Bush-Mosteller)
@@ -70,14 +77,17 @@ class AspirationBMPGGAgent:
         p_init : float = 0.5, # initial tendency to contribute
         sigma : float = 0.2, # stimulus
         max_redraws : int = 10000, # redraw max (to not loop forever but keep long enough)
+        eta : float = 0.0 # Dynamic aspiration (0.0 = aspiration fix like in the paper)
     ):
     
         self.A = A
+        self.A_init = A
         self.beta = beta
         self.coop_threshold = coop_threshold
         self.p_init = p_init
         self.sigma = sigma
         self.max_redraws = max_redraws
+        self.eta = eta
 
         self.p = fix01(p_init)
         self.last_action = None
@@ -117,17 +127,21 @@ class AspirationBMPGGAgent:
     def update(self, reward : float):
         
         if self.last_action is None:
-            raise RuntimeError("update() called before select_action().")
+            raise RuntimeError("update called before select_action")
 
-        s_prev = stimulus(r=float(reward), A=self.A, beta=self.beta)
+        s_prev = stimulus(r = reward, A = self.A, beta = self.beta)
         action_prev = C if self.last_action >= self.coop_threshold else D
-        self.p = bm_update(p=self.p, action_prev=action_prev, s_prev=s_prev)
+        self.p = bm_update(p = self.p, action_prev = action_prev, s_prev = s_prev)
+        
+        if self.eta > 0:
+            self.A = (1 - self.eta) * self.A + self.eta * reward
 
 
     # It reset all value
     def reset(self):
         self.p = fix01(self.p_init)
         self.last_action = None
+        self.A = self.A_init
 
 
 
